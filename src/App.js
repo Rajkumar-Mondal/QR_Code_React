c14c11ee33e11e846f2086e46fc5d30fece6055a
Rajkumar <rajkumar.mondal@varatechnology.com>
2022-07-15 18:14:14 +0530

    Feature: QR code Scanner and Generator Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import QRCode from 'qrcode';
+import QrReader from 'react-qr-reader'
 
 function App() {
+  const [text, setText] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [scanResultFile, setScanResultFile] = useState('');
+  // const [scanResultWebCam, setScanResultWebCam] = useState('');
+  const qrRef = useRef(null);
+
+
+
+
+  const generateQrCode = async () => {
+    try {
+      const response = await QRCode.toDataURL(text);
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const handleErrorFile = (error) => {
+    console.log(error);
+  }
+  const handleScanFile = (result) => {
+    if (result) {
+      setScanResultFile(result);
+    }
+  }
+  const onScanFile = () => {
+    qrRef.current.openImageDialog();
+  }
+  // const handleErrorWebCam = (error) => {
+  //   console.log(error);
+  // }
+  // const handleScanWebCam = (result) => {
+  //   if (result) {
+  //     setScanResultWebCam(result);
+  //   }
+  // }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container1'>
+        <div>
+          <input onChange={(e) => setText(e.target.value)} />
+          <button onClick={() => generateQrCode()}>Generate</button>
+        </div>
+        <div>
+          {imageUrl ? (
+            <a href={imageUrl} download>
+              <img src={imageUrl} alt="img" />
+            </a>) : null}
+        </div>
+      </div>
+
+      <div className='container2'>
+        <button onClick={() => onScanFile()}>Scan Qr Code</button>
+        <QrReader
+          ref={qrRef}
+          delay={300}
+          style={{ width: '100%' }}
+          onError={handleErrorFile}
+          onScan={handleScanFile}
+          legacyMode
+        />
+        <h3>Scanned Code: {scanResultFile}</h3>
+      </div>
+
     </div>
   );
 }
